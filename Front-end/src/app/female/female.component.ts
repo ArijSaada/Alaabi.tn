29859162b0cfb8d19675f87815da9ce59824c77b
ArijSaada <arij.saada.dev@gmail.com>
2026-01-30 23:34:57 +0100

    Add files via upload

---
+++ Front-end/src/app/female/female.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-female',
+  standalone: true,
+  imports: [],
+  templateUrl: './female.component.html',
+  styleUrl: './female.component.css'
+})
+export class FemaleComponent {
+
+}
