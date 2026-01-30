1a2fcc5ec30ac9a6ab6cc9868ec2a5be96db5375
ArijSaada <arij.saada.dev@gmail.com>
2026-01-30 23:37:29 +0100

    Add files via upload

---
+++ Front-end/src/app/nouveautes/nouveautes.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nouveautes',
+  standalone: true,
+  imports: [],
+  templateUrl: './nouveautes.component.html',
+  styleUrl: './nouveautes.component.css'
+})
+export class NouveautesComponent {
+
+}
