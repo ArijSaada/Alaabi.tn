3c28dd787b9cceeb11aaa4cf0e6d972f1f7c6bef
ArijSaada <arij.saada.dev@gmail.com>
2026-01-30 23:33:56 +0100

    Add files via upload

---
+++ Front-end/src/app/dolls/dolls.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-female',
+  standalone: true,
+  imports: [],
+  templateUrl: './dolls.component.html',
+  styleUrl: './dolls.component.css'
+})
+export class DollsComponent {
+
+}
---
+++ Front-end/src/app/dolls/dolls.service.ts
@@ -0,0 +1,37 @@
+/*import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable} from 'rxjs';
+//ki tsta3ml httpclient fi angular les données yjiwk mil back fi wist observable hedheka a3leh on l'importe , bch nt7akmou feha 
+
+@Injectable({
+  providedIn: 'root'
+}) 
+/*
+Cette ligne indique à Angular que ce service peut être injecté 
+dans n'importe quel composant ou service de l'application
+ et qu'il doit être fourni au niveau racine (root).
+
+export class DollsService {
+  getDollsCategoryId() {
+    throw new Error('Method not implemented.');
+  }
+  private url = 'http://localhost:3000/routes/userInterface';
+  constructor(private http: HttpClient) {
+    /*  Déclare un constructeur pour le service. 
+    Angular injecte automatiquement une instance de HttpClient
+     dans ce service, 
+     ce qui nous permettra d'effectuer des requêtes HTTP.
+    
+
+
+
+
+   }
+  getDolls(): Observable<any[]> {
+    return this.http.get<any[]>(this.url);
+//renvoie une liste de données depuis le backend
+
+ 
+}
+}
+*/
