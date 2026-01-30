3410a59ce9d0edb9e5d439797b3920dbd3706648
ArijSaada <arij.saada.dev@gmail.com>
2026-01-30 23:23:03 +0100

    Add files via upload

---
+++ Front-end/src/app/admin-page/admin-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+@Component({
+  selector: 'app-admin-page',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './admin-page.component.html',
+  styleUrl: './admin-page.component.css'
+})
+export class AdminPageComponent {
+
+}
