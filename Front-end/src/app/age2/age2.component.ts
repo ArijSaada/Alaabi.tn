eea1ccc1bf76c7e7682b31720721605139349e05
ArijSaada <arij.saada.dev@gmail.com>
2026-01-30 23:25:08 +0100

    Add files via upload

---
+++ Front-end/src/app/age2/age2.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-age2',
+  standalone: true,
+  imports: [],
+  templateUrl: './age2.component.html',
+  styleUrl: './age2.component.css'
+})
+export class Age2Component {
+
+}
