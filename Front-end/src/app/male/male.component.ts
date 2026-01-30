5ccddb867f496a10709a60e84e4e2a66d34fbb9a
ArijSaada <arij.saada.dev@gmail.com>
2026-01-30 23:35:44 +0100

    Add files via upload

---
+++ Front-end/src/app/male/male.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-male',
+  standalone: true,
+  imports: [],
+  templateUrl: './male.component.html',
+  styleUrl: './male.component.css'
+})
+export class MaleComponent {
+
+}
